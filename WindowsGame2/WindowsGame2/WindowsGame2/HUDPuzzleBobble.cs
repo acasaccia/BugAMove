94e0f6c30444f9b44071ae133998caa2f70f91f4
Andrea Casaccia <acasaccia@gmail.com>
2012-03-29 23:40:20 +0000

    Aspetto menù, suoni in game, tremolio prima della discesa del tetto.

--- WindowsGame2/WindowsGame2/WindowsGame2/HUDPuzzleBobble.cs
+++ WindowsGame2/WindowsGame2/WindowsGame2/HUDPuzzleBobble.cs
@@ -24,15 +24,25 @@ namespace WindowsGame2
             this.statsTitleColor = Color.White;
             this.statsColor = Color.White;
            
-            this.statsPosition1UP =  new Vector2( 0.02f * this.game.graphics.PreferredBackBufferWidth, 0.02f * this.game.graphics.PreferredBackBufferHeight);
+            this.statsPosition1UP =  new Vector2( 0.035f * this.game.graphics.PreferredBackBufferWidth, 0.02f * this.game.graphics.PreferredBackBufferHeight);
         }
         public override void Initialize()
         {
             base.Initialize();
         }
+
+
+        Texture2D hud, hud2;
+        Rectangle rect;
+        Rectangle rect2;
+
         protected override void LoadContent()
         {
-            
+            hud = game.Content.Load<Texture2D>("hud");
+            hud2 = game.Content.Load<Texture2D>("hud2");
+            rect = new Rectangle(10, 20, 175, 100);
+            rect2 = new Rectangle(this.game.GraphicsDevice.Viewport.Width - 185, 20, 175, 100);
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             this.timeFont = game.Content.Load<SpriteFont>("BigFont");
@@ -43,16 +53,22 @@ namespace WindowsGame2
         {
             base.Dispose(disposing);
         }
+
         public override void Draw(GameTime gameTime)
         {
+            //spriteBatch.Draw(hud, Vector2.Zero, Color.White);
+
             int time = (int)PuzzleBobble.game_state.LevelStatus.ElapsedTime.Value;
             string timeString = String.Format("{0:D2}:{1:D2}", (int)time/60, time % 60);
 
             int score1UP = PuzzleBobble.game_state.LevelStatus.Score.Value; 
             //int score1UP = PuzzleBobble.game_state.Value.LevelStatus.Score.Value; // LevelStatus.Score.Value;
-            Vector2 timeStringPosition = new Vector2(0.825f * this.game.graphics.PreferredBackBufferWidth, 0.02f * this.game.graphics.PreferredBackBufferHeight);
+            Vector2 timeStringPosition = new Vector2(0.85f * this.game.graphics.PreferredBackBufferWidth, 0.02f * this.game.graphics.PreferredBackBufferHeight);
             float delta = this.statsFont.MeasureString("UP").Y;
             spriteBatch.Begin();
+
+            spriteBatch.Draw(hud, rect, Color.White);
+            spriteBatch.Draw(hud2, rect2, Color.White);
 
             spriteBatch.DrawString(this.timeFont, "Time:\n" + timeString, timeStringPosition, this.timeColor, 0.0f, new Vector2(0, 0), new Vector2(1, 1), SpriteEffects.None, 0);
             //spriteBatch.DrawString(this.statsFont, "--- 1UP STATS --- ", this.statsPosition1UP, this.statsTitleColor,
